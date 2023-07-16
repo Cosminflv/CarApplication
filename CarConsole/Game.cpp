442d4be50c23e2afa730d3791ae1c8c3f1aa4952
Cosmin <cosmin_popovici2012@yahoo.com>
2023-07-16 16:57:38 +0300

    Revert "Added menu."
    This reverts commit 3667fbb609b069741ee97620be59d9dcd1032a72.

--- CarConsole/Game.cpp
+++ CarConsole/Game.cpp
@@ -1,27 +1,13 @@
 #include "Game.h"
 #include <iostream>
+void StartGame()
+{
+	std::cout << "The game has started!\n";
+}
 
 void Game::StartGame()
 {
-	int op = -1;
-	std::cout << "The game has started!\n 1-Deploy a car\n 2-Enter car's proprieties\n 3-Delete car\n";
-	while (op)
-	{
-		switch (op)
-		{
-		case 1:
-			std::cout << "Enter car's type:";
-				//GenerateCarVector();
-				break;
-		case 2:
-			std::cout << "Select proriety:";
-			break;
-		case 3:
-			//DeleteCar();
-			break;
-
-		}
-	}
+	std::cout << "The game has started!\n";
 }
 
 Car Game::car1(ECarType type, EColor color) 
